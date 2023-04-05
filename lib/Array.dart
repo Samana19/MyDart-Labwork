cb3a00fe8b4196caa4f2651f25d124619501df40
Samana19 <samanashrestha02@gmail.com>
2023-04-05 19:04:32 +0545

    Array

---
+++ lib/Array.dart
@@ -0,0 +1,16 @@
+//print elements in array usinf loop
+// void main(){
+//   var arr=[1,4,3,8,10];
+//   for (int i=0;i<arr.length;i++){
+//     print(arr[i]);
+//   }
+
+// }
+
+void main(){
+  var arr=[1,2,3,4,5];
+  for (int i=0;i<arr.length;i++){
+    if(i%2==0){
+      print(arr[i]);
+    }
+  }}

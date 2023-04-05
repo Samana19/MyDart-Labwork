fb786154cd76e61e806bd6b7365e2a6571dfe885
Samana19 <samanashrestha02@gmail.com>
2023-04-05 19:06:51 +0545

    Print name 100 times

---
+++ lib/Lab1/printname.dart
@@ -0,0 +1,11 @@
+//Write a dart program to print your name 100 times.
+void main() {
+  String name = "Samana";
+  for (int i = 0; i <= 100; i++) {
+    print(printName(name));
+  }
+}
+
+String printName(String name) {
+  return name;
+}

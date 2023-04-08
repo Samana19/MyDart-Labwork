14df6ec7bf94fef0524b47eb3bc9699e991ba7ed
Samana19 <samanashrestha02@gmail.com>
2023-04-08 17:10:58 +0545

    keyLength=4

---
+++ lib/Lab2/mapLength.dart
@@ -0,0 +1,12 @@
+//Create a map with a name, and phone keys and store some values. Use where to find all keys that have length 4
+
+void main() {
+  var names = {
+    'name': 'Samana',
+    'phone': 9840000000,
+    'age': 20,
+    'country': 'Nepal'
+  };
+  var mapLength = names.keys.where((key) => key.length == 4);
+  print(mapLength);
+}
